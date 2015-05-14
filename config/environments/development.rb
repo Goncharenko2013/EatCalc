afbf9ca3b104d7c6e8c6e647b50b9ec49ec5fca7
Tetyana2015 <tgoncharenko@gmail.com>
2015-05-14 15:33:52 +0300

    roughly styled all view & mailing data in dev&prod env

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -42,4 +42,17 @@ Rails.application.configure do
   # install Devise
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+      address: 'smtp.gmail.com',
+      port: 587,
+      domain: ENV['gmail.com'],
+      authentication: 'plain',
+      enable_starttls_auto: true,
+      user_name: ENV['GMAIL_USERNAME'],
+      password: ENV['GMAIL_PASSWORD']
+  }
+
 end
