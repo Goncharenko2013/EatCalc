1c3f75c4dfd41003b1b7acc1a0990c3969b1e53b
Tetyana2015 <tgoncharenko@gmail.com>
2015-05-21 14:45:46 +0300

    articles created

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,14 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   ACTIVITIES = [ 'Activity low', 'Activity medium', 'Activity high' ]
+
+  validates :height, :presence => true, :inclusion => { :in => 100..220, :message => "The height must be between 100 and 220" }
+  validates :weight, :presence => true, :inclusion => { :in => 40..220, :message => "The weight must be between 40 and 220" }
+  validates :age, :presence => true, :inclusion => { :in => 15..70, :message => "The age must be between 15 and 70" }
+
+  validates_inclusion_of :gender, in: %w( 'Male', 'Female' )
+
+  validates_inclusion_of :lifestyle, in: %w( 'Activity low', 'Activity medium', 'Activity high' )
+
+
 end
