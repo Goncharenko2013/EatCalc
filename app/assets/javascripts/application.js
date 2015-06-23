d61dc2c86834630658a3cbdcdea979ce0409eaa9
Tetyana2015 <tgoncharenko@gmail.com>
2015-06-23 20:59:18 +0300

    tinymce added

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,10 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require tinymce
 //= require_tree .
+
+tinyMCE.init({
+    mode : "specific_textareas",
+    editor_selector : "mce_editor"
+});
