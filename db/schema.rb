df1279d2c53acb364a2b0e5ee77687d482727ad8
Tetyana2015 <tgoncharenko@gmail.com>
2015-07-02 19:49:24 +0300

    calorie model created, import from Excel file added to seed.rb

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150603162157) do
+ActiveRecord::Schema.define(version: 20150702161334) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
@@ -51,6 +51,18 @@ ActiveRecord::Schema.define(version: 20150603162157) do
     t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "calories", force: :cascade do |t|
+    t.string   "product"
+    t.integer  "water"
+    t.integer  "proteins"
+    t.integer  "fats"
+    t.integer  "carbohydrates"
+    t.integer  "ccal"
+    t.string   "category_id"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
   end
 
   create_table "users", force: :cascade do |t|
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+require 'roo'
+
+def fetch_excel_data
+  ex = Excel.new("#{Rails.root}/public/productCalories.xlsx")
+  ex.default_sheet = ex.sheets[1] #Mention the sheet number
+  2.upto(1000) do |line| #start and end of row
+    product = ex.cell(line,'A')
+    water = ex.cell(line,'B')
+    proteins = ex.cell(line,'C')
+    fats = ex.cell(line,'D')
+    carbohydrates = ex.cell(line,'E')
+    ccal = ex.cell(line,'F')
+    category_id = ex.cell(line,'G')
+
+    @calorie = Calorie.create(:product => product,:water => water,:proteins => proteins, :fats => fats, :carbohydrates => carbohydrates, :ccal => ccal, :category_id => category_id)
+  end
+end
