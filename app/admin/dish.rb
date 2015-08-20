03439e645ecd1361f4ba133a47ab0bce7ab4b14f
Tetyana2015 <tgoncharenko@gmail.com>
2015-08-20 14:32:21 +0300

    nested_attributes added

--- app/admin/dish.rb
+++ app/admin/dish.rb
@@ -1,17 +1,7 @@
 ActiveAdmin.register Dish do
 
-# See permitted parameters documentation:
-# https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-#
-# permit_params :list, :of, :attributes, :on, :model
-#
-# or
-#
-# permit_params do
-#   permitted = [:permitted, :attributes]
-#   permitted << :other if resource.something?
-#   permitted
-# end
 
+permit_params :name, :id, roles: []
+#accepts_nested_attributes_for :products, :dish_products
 
 end
--- app/models/dish.rb
+++ app/models/dish.rb
@@ -1,4 +1,5 @@
 class Dish < ActiveRecord::Base
   has_many :dish_products
   has_many :products, through: :dish_products
+  accepts_nested_attributes_for :products, :dish_products
 end
