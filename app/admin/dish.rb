ee9e8d9790d6b42dc759a7898a60a6b71a31f78c
Tetyana2015 <tgoncharenko@gmail.com>
2015-08-26 18:07:54 +0300

    form in activeAdmin_dish added

--- app/admin/dish.rb
+++ app/admin/dish.rb
@@ -4,4 +4,13 @@ ActiveAdmin.register Dish do
 permit_params :name, :id, roles: []
 #accepts_nested_attributes_for :products, :dish_products
 
-end
+  form do |f|
+    f.inputs 'Dish' do
+      f.input :name
+      f.input :products, as: :check_boxes, collection: Product.all
+
+      #f.input :weight
+    end
+    f.actions
+  end
+  end
--- app/models/dish.rb
+++ app/models/dish.rb
@@ -1,5 +1,5 @@
 class Dish < ActiveRecord::Base
   has_many :dish_products
   has_many :products, through: :dish_products
-  accepts_nested_attributes_for :products, :dish_products
+  accepts_nested_attributes_for :dish_products
 end
