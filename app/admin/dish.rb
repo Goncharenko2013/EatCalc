66f5b78655e113c40831b95ac2f2177613998310
Tetyana2015 <tgoncharenko@gmail.com>
2015-08-29 17:16:17 +0300

    dish name presence validation & placeholders added

--- app/admin/dish.rb
+++ app/admin/dish.rb
@@ -5,12 +5,12 @@ permit_params :name, :id, dish_products_attributes: [:product_id, :weight]
 
   form do |f|
     f.inputs 'Dish' do
-      f.input :name
+      f.input :name, placeholder: 'insert name of your own dish'
       f.inputs "Products" do
         f.has_many :dish_products, heading: false, allow_destroy: true do |dp|
           dp.input :product_id, :as => :select, :collection => option_groups_from_collection_for_select(ProductCategory.all, :products, :name, :id, :product)
           # dp.input :product_id, :as => :select, :collection => Product.all.pluck(:product, :id)
-          dp.input :weight
+          dp.input :weight, placeholder: 'insert component product weight in gram'
         end
       end
     end
--- app/models/dish.rb
+++ app/models/dish.rb
@@ -2,4 +2,8 @@ class Dish < ActiveRecord::Base
   has_many :dish_products
   has_many :products, through: :dish_products
   accepts_nested_attributes_for :dish_products
+
+  validates :name, :presence => true
+
+
 end
